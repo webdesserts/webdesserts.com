29972cec960a58f0ba77b4fd0a70f5e33cd6bfd7
Michael Mullins <michael@webdesserts.com>
2019-06-05 13:23:09 -0500

    rework build step
    - use babel
    - add svg support
    - async typechecking
    - a bit faster now

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
 let path = require('path')
+let ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
   output: { path: path.resolve("assets/build") },
   devtool: "sourcemaps",
   mode: "development",
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: true,
+      useTypescriptIncrementalApi: true,
+      checkSyntacticErrors: true,
+      silent: true,
+      formatter: 'codeframe'
+}),
+  ],
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      {
+        test: /\.svg$/,
+        use: [
+          { loader: "babel-loader" },
+          {
+            loader: "@svgr/webpack",
+            options: { babel: false }
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
     ]
   }
-}
+};
